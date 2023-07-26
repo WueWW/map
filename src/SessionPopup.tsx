a4ff50b7ab100af8ade03580e5a9ca761922a289
Stefan Siegl <stesie@brokenpipe.de>
2023-07-26 19:51:36 +0200

    fix address display

--- src/SessionPopup.tsx
+++ src/SessionPopup.tsx
@@ -7,29 +7,29 @@ export interface SessionPopupProps {
     sessions: Session[];
 }
 
-const SessionPopup = ({ sessions }: SessionPopupProps) => {
-    const parts = sessions[0]
-        .location!.name.split(/\n/)
-        .map((x: string) => x.trim())
-        .filter((x: string) => x);
-
-    return (
-        <Popup>
-            <p className="session location name">{parts[0]}</p>
-            <p className="session location address">{parts.slice(1).join(", ")}</p>
-            <hr />
-            {[...sessions]
-                .sort((a, b) => a.start.localeCompare(b.start))
-                .map((session, index) => (
-                    <Fragment key={index}>
-                        <h3 className="session title">{session.title}</h3>
-                        <p className="session host">
-                            {session.host.name} @ {formatDate(session.start)}
-                        </p>
-                    </Fragment>
-                ))}
-        </Popup>
-    );
-};
+const SessionPopup = ({ sessions }: SessionPopupProps) => (
+    <Popup>
+        <p className="session location name">{sessions[0].location?.name}</p>
+        <p className="session location address">
+            {sessions[0].location?.streetNo}
+            {sessions[0].location?.city !== "Würzburg" && (
+                <>
+                    , {sessions[0].location?.zipcode} {sessions[0].location?.city}
+                </>
+            )}
+        </p>
+        <hr />
+        {[...sessions]
+            .sort((a, b) => a.start.localeCompare(b.start))
+            .map((session, index) => (
+                <Fragment key={index}>
+                    <h3 className="session title">{session.title}</h3>
+                    <p className="session host">
+                        {session.host.name} @ {formatDate(session.start)}
+                    </p>
+                </Fragment>
+            ))}
+    </Popup>
+);
 
 export default SessionPopup;
